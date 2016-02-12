4ec9abaf6ac9423f55d0fa5ea44eb99d6ec21265
nellid <janellemichaelis@gmail.com>
2016-02-11 16:25:55 -0800

    Clicked Folder Disappears
    Clicked Folder Disappears

---
+++ Stressident/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	// The game controller subscribes to these events from other classes
+	void OnEnable()
+	{	
+		GameView.folderClicked += GetQuestion;
+	}
+
+	void OnDisable()
+	{
+		GameView.folderClicked -= GetQuestion;
+	}
+
+	string GetQuestion()
+	{
+		// Call the method in the game model that generates questions
+		return "Is this the best question anyone has ever asked you?";
+	}
+}
---
+++ Stressident/Assets/Scripts/GameView.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameView : MonoBehaviour 
+{
+	// The folder clicked event fires an alert to the game controller
+	public delegate string FolderClicked();
+	public static event FolderClicked folderClicked;
+
+	void Update () 
+	{
+		// If user clicks something ..
+		if ( Input.GetMouseButtonDown(0))
+		{
+			// Use raycasting to determine which object the player clicked
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray, out hit, 100.0f))
+			{  
+				// If the user clicked a folder ...
+				if (hit.collider.gameObject.tag == "Folder") 
+				{
+					string question = "Did you know that if you're seeing this question then something went wrong?";
+
+					if (folderClicked != null) 
+					{
+						question = folderClicked ();
+					}
+
+					Debug.Log (question);
+					Destroy (GameObject.Find (hit.collider.gameObject.name));
+				}
+			}
+		}
+	}
+}

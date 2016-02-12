160804bc82926cd4c211c1b08d9652488aa19e78
u0498279 <u0498279@1110mac-38.lib.utah.edu>
2016-02-11 19:05:54 -0700

    DATABASE MADNESS

--- Stressident/Assets/Scripts/GameController.cs
+++ Stressident/Assets/Scripts/GameController.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour 
 {
+	public GameObject db;
+	public Google2u.Sheet1 db1;
+	void Start() {
+		if (db = null) {
+			db = GameObject.Find ("Google2uDatabase");
+	
+			db1 = db.GetComponent<Google2u.Sheet1>();
+			
+		}
+	}
 	// The game controller subscribes to these events from other classes
 	void OnEnable()
 	{	
@@ -16,7 +27,11 @@ public class GameController : MonoBehaviour
 
 	string GetQuestion()
 	{
+		Google2u.Sheet1Row a = db1.Rows [(int)Google2u.Sheet1.rowIds.ID_Q1];
+		Debug.Log(a[0]);
+		Debug.Log (db1.Rows.GetType ());
+		Debug.Log (db1.Rows [(int)Google2u.Sheet1.rowIds.ID_Q1]);
 		// Call the method in the game model that generates questions
-		return "Is this the best question anyone has ever asked you?";
+		return a[0];
 	}
 }

6f22e92e52ca91f2bb556aa1c72a8fe364c0bad2
u0498279 <u0498279@1110mac-28.lib.utah.edu>
2016-02-25 18:53:50 -0700

    New Game scene

---
+++ Stressident/Assets/NewGameCont.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGameCont : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NewGame() {
+		Application.LoadLevel (1);
+
+	}
+}

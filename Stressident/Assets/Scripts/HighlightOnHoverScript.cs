6df2869cdeb56a8f24b45af327ae89e121ac3259
nellid <janellemichaelis@gmail.com>
2016-03-06 10:19:58 -0700

    Objects highlight on hover
    Objects highlight on hover

---
+++ Stressident/Assets/Scripts/HighlightOnHoverScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightOnHoverScript : MonoBehaviour 
+{
+	private Color originalColor;
+
+	void OnMouseEnter()
+	{
+		originalColor = GetComponent<Renderer>().material.color;
+		GetComponent<Renderer>().material.color = Color.yellow;
+	}
+
+	void OnMouseExit()
+	{
+		GetComponent<Renderer>().material.color = originalColor;
+	}
+}

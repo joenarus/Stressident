c0583a9bf7ce95e00ce5716e97cd260bc886a2de
u0498279 <u0498279@kcmac-33.scl.utah.edu>
2016-03-17 16:50:12 -0600

    BASKETBALL YEAHHHHHHH

--- Stressident/Assets/Scripts/Basket.cs
+++ Stressident/Assets/Scripts/Basket.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class Basket : MonoBehaviour
 {
 	public GameObject score; //reference to the ScoreText gameobject, set in editor
-	
+	public int tempScore = 0;
+
 	void OnCollisionEnter() //if ball hits board
 	{
 
@@ -12,9 +13,12 @@ public class Basket : MonoBehaviour
 	
 	void OnTriggerEnter(Collider other) //if ball hits basket collider
 	{
-		if (other.tag == "BasketBall") {
-			Debug.Log("YYOOOOOOO");
+		if (other.tag == "Basketball") {
+			tempScore++;
+			GameObject.Destroy(GameObject.Find("Ball(clone)"));
 		}
+
+		score.GetComponent<Text> ().text = tempScore.ToString();
 		/*
 		int currentScore = int.Parse(score.GetComponent().text) + 1; //add 1 to the score
 		score.GetComponent().text = currentScore.ToString();
--- Stressident/Assets/Scripts/BasketBall.cs
+++ Stressident/Assets/Scripts/BasketBall.cs
@@ -6,7 +6,7 @@ public class BasketBall : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject ball; //reference to the ball prefab, set in editor
-	private Vector3 throwSpeed = new Vector3(0, 0, 0); //This value is a sure basket, we'll modify this using the forcemeter
+	private Vector3 throwSpeed = new Vector3(0, 10, 10); //This value is a sure basket, we'll modify this using the forcemeter
 	public Vector3 ballPos; //starting ball position
 	private bool thrown = false; //if ball has been thrown, prevents 2 or more balls
 	private GameObject ballClone; //we don't use the original prefab
@@ -25,8 +25,7 @@ public class BasketBall : MonoBehaviour {
 	{
 		if (enabled) {
 			/* Increase Gravity */
-			Physics.gravity = new Vector3 (0, -20, 0);
-			ballPos = new Vector3(player.transform.position.x, player.transform.position.y, 0); 
+			//Physics.gravity = new Vector3 (0, -20, 0);
 		}
 	}
 
@@ -49,23 +48,20 @@ public class BasketBall : MonoBehaviour {
 			meter.value = currentForce;
 
 			if(Input.GetButton("Fire1") && !thrown) {
-				ballClone = Instantiate(ball, ballPos, transform.rotation) as GameObject;
-				throwSpeed.x = throwSpeed.x + currentForce;
-				throwSpeed.y = throwSpeed.y + currentForce;
-				throwSpeed.z = throwSpeed.z + currentForce;
-				ballClone.GetComponent<Rigidbody>().AddForce(throwSpeed, ForceMode.Impulse);
-				Debug.Log (ballClone.transform.position);
-				Debug.Log (throwSpeed);
+
+				Vector3 theVector =  Input.mousePosition;
+				theVector.z = 1;
+				ballClone = Instantiate(ball, Camera.main.ScreenToWorldPoint(theVector),Quaternion.LookRotation(Camera.main.ScreenToWorldPoint(theVector))) as GameObject;
+				ballClone.GetComponent<Rigidbody>().AddForce(player.transform.forward * 10, ForceMode.Impulse);
 				thrown = true;
 			}
 
-			if (ballClone != null && ballClone.transform.position.y < -10)
+			if (ballClone != null && ballClone.transform.position.y < .2)
 			{
 				Destroy(ballClone);
 				thrown = false;
-				throwSpeed = new Vector3(0, 0, 0);
+				throwSpeed = new Vector3(0, 5, 5);
 			}
 		}
 	}
-
 }
